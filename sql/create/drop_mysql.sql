e6e01746dc0da53ada42dbe9bf3cb366ceeaeee9
Alex <alex1kiss@gmail.com>
2015-12-02 15:12:48 +0200

    2015-12-02

--- sql/create/drop_mysql.sql
+++ sql/create/drop_mysql.sql
@@ -1,3 +1,22 @@
+REVOKE ALL PRIVILEGES ON * . * FROM 'root'@'localhost';
+
+REVOKE ALL PRIVILEGES ON `world` . * FROM 'root'@'localhost';
+
+REVOKE GRANT OPTION ON `world` . * FROM 'root'@'localhost';
+
+REVOKE ALL PRIVILEGES ON `characters` . * FROM 'root'@'localhost';
+
+REVOKE GRANT OPTION ON `characters` . * FROM 'root'@'localhost';
+
+REVOKE ALL PRIVILEGES ON `auth` . * FROM 'root'@'localhost';
+
+REVOKE GRANT OPTION ON `auth` . * FROM 'root'@'localhost';
+
+REVOKE ALL PRIVILEGES ON `hotfixes` . * FROM 'root'@'localhost';
+
+REVOKE GRANT OPTION ON `hotfixes` . * FROM 'root'@'localhost';
+
+DROP USER 'root'@'localhost';
 
 DROP DATABASE IF EXISTS `auth`;
 

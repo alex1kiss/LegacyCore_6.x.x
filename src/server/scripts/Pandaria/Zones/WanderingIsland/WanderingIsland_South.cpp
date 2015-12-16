13b799e6222e2dce932cc5f6674c7ae19ef9c989
Alex <alex1kiss@gmail.com>
2015-12-16 13:33:14 +0200

    Working at pandaria

--- src/server/scripts/Pandaria/Zones/WanderingIsland/WanderingIsland_South.cpp
+++ src/server/scripts/Pandaria/Zones/WanderingIsland/WanderingIsland_South.cpp
@@ -1,8 +1,31 @@
+/*
+ * Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2013-2015 LegacyCore <http://www.dar-project.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
 #include "ScriptedEscortAI.h"
+#include "Player.h"
+#include "WorldSession.h"
 
-
+/*######
+## npcs_dithers_and_arbington
+######*/
 
 #define GOSSIP_CHOOSE_FACTION     "I'm ready to choose my destiny."
 #define GOSSIP_TP_STORMIND        "I would like to go to Stormwind"

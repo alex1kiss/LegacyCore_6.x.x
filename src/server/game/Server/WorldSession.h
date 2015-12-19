d7ec9078ee2a50b24d2d1784c7bbbad5551d4dea
Alex <alex1kiss@gmail.com>
2015-12-19 11:35:15 +0200

    Working at pandaria

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -1079,7 +1079,7 @@ class WorldSession
         void HandlePlayedTime(WorldPackets::Character::RequestPlayedTime& packet);
 
         // Pandaren Faction Choice
-        void HandlePandarenFactionChoiceOpcode(WorldPacket& recvData);
+        void HandlePandarenFactionChoiceOpcode(recvPacket);
         
         // new
         void HandleLookingForGroup(WorldPacket& recvPacket);
